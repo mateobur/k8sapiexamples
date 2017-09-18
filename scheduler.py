421cecd345bb082dd74d8515ac87e6112a105545
Mateo Burillo <mateobur@gmail.com>
2017-09-18 09:47:52 +0200

    pep8

--- scheduler.py
+++ scheduler.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-import time
-import random
 import json
 
 from kubernetes import client, config, watch
@@ -9,28 +7,32 @@ from sdcclient import SdcClient
 
 
 config.load_kube_config()
-v1=client.CoreV1Api()
+v1 = client.CoreV1Api()
 metric_id = "net.http.request.time"
 sdclient = SdcClient("c30a0cfd-609a-41a6-b822-e8e0d4bdeb0d")
-metrics = [{ "id": metric_id, "aggregations": { "time": "timeAvg", "group": "avg" } }]
+metrics = [{"id": metric_id, "aggregations":
+           {"time": "timeAvg", "group": "avg"}}]
 node_blacklist = ["kubemaster"]
 
 scheduler_name = "sysdigsched"
 
+
 def get_request_time(hostname):
     hostfilter = "host.hostName = '%s'" % hostname
     metricdata = sdclient.get_data(metrics, -60, 0, 60, hostfilter)
-    request_time = float(metricdata[1].get('data')[0].get('d')[0])
+    request_time = float(metricdata[1].get("data")[0].get("d")[0])
     print hostname + " (" + metric_id + "): " + str(request_time)
     return request_time
+
 
 def best_request_time(nodes):
     if not nodes:
         return []
     node_times = [get_request_time(hostname) for hostname in nodes]
     best = nodes[node_times.index(min(node_times))]
-    print "best node: " + best 
+    print "best node: " + best
     return best
+
 
 def nodes_available():
     ready_nodes = []
@@ -43,32 +45,34 @@ def nodes_available():
     print "Nodes available: " + str(ready_nodes)
     return ready_nodes
 
+
 def scheduler(name, node, namespace="default"):
-    body=client.V1Binding()
-        
-    target=client.V1ObjectReference()
-    target.kind="Node"
-    target.apiVersion="v1"
-    target.name= node
-    
-    meta=client.V1ObjectMeta()
-    meta.name=name
-    
-    body.target=target
-    body.metadata=meta
-    
+    body = client.V1Binding()
+    target = client.V1ObjectReference()
+    target.kind = "Node"
+    target.apiVersion = "v1"
+    target.name = node
+
+    meta = client.V1ObjectMeta()
+    meta.name = name
+
+    body.target = target
+    body.metadata = meta
+
     return v1.create_namespaced_binding(namespace, body)
+
 
 def main():
     w = watch.Watch()
     for event in w.stream(v1.list_namespaced_pod, "default"):
-        if event['object'].status.phase == "Pending" and \
-           event['object'].spec.scheduler_name == scheduler_name and not \
-           event['object'].spec.node_name: 
+        if event["object"].status.phase == "Pending" and \
+           event["object"].spec.scheduler_name == scheduler_name and not \
+           event["object"].spec.node_name:
             try:
-                res = scheduler(event['object'].metadata.name, best_request_time(nodes_available()))
+                res = scheduler(event["object"].metadata.name,
+                                best_request_time(nodes_available()))
             except client.rest.ApiException as e:
-                print json.loads(e.body)['message']
-                    
-if __name__ == '__main__':
+                print json.loads(e.body)["message"]
+
+if __name__ == "__main__":
     main()
